a1a61d5fd38419bc8edbc3df43557ae5a2409b1d
chiki-06 <8xxxchikixxx8@gmail.com>
2025-11-13 20:44:23 -0300

    tomar item con E

--- Assets/ChikiAssets/Scripts/Scriptsinventory/Inventory.cs
+++ Assets/ChikiAssets/Scripts/Scriptsinventory/Inventory.cs
@@ -7,46 +7,28 @@ using UnityEngine.Rendering;
 public class Inventory : MonoBehaviour
 {
     [Header("References")]
-    [SerializeField]
-    InventoryUI ui;
-    [SerializeField]
-    AudioSource audioSource;
+    [SerializeField] InventoryUI ui;
+    [SerializeField] AudioSource audioSource;
 
     [Header("Prefabs")]
-    [SerializeField]
-    GameObject droppedItemPrefab;
+    [SerializeField] GameObject droppedItemPrefab;
 
     [Header("Audio Clips")]
-    [SerializeField]
-    AudioClip pickUpItemAudio;
-    [SerializeField]
-    AudioClip dropItemAudio;
+    [SerializeField] AudioClip pickUpItemAudio;
+    [SerializeField] AudioClip dropItemAudio;
 
     [Header("State")]
-    [SerializeField]
-    SerializedDictionary<string, Item> inventory = new();
+    [SerializeField] SerializedDictionary<string, Item> inventory = new();
 
-    public void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("DroppedItem"))
-        {
-            var droppedItem = other.GetComponent<DroppedItem>();
-            if (droppedItem.pickedUp)
-            {
-                return;
-            }
-            droppedItem.pickedUp = true;
-            AddItem(droppedItem.item);
-            Destroy(other.gameObject);
-            audioSource.PlayOneShot(pickUpItemAudio);
-        }
-    }
 
-    void AddItem(Item item)
+    public void AddItemFromWorld(Item item)
     {
         var inventoryId = Guid.NewGuid().ToString();
         inventory.Add(inventoryId, item);
+
         ui.AddUIItem(inventoryId, item);
+
+        audioSource.PlayOneShot(pickUpItemAudio);
     }
 
     public void DropItem(string inventoryId)
@@ -54,8 +36,10 @@ public class Inventory : MonoBehaviour
         var droppedItem = Instantiate(droppedItemPrefab, transform.position, Quaternion.identity).GetComponent<DroppedItem>();
         var item = inventory.GetValueOrDefault(inventoryId);
         droppedItem.Initialize(item);
+
         inventory.Remove(inventoryId);
         ui.RemoveUIItem(inventoryId);
+
         audioSource.PlayOneShot(dropItemAudio);
     }
 
@@ -64,11 +48,8 @@ public class Inventory : MonoBehaviour
         foreach (var item in inventory.Values)
         {
             if (item.id == itemId)
-            {
                 return true;
-            }
         }
         return false;
     }
-
 }
